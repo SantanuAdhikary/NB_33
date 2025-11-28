b9b3cac4b92901604e5d833d355073ad837a3eb9
SantanuAdhikary <santanuadhikary13@gmail.com>
2025-11-28 13:43:29 +0530

    signup done

---
+++ JAVASCRIPT/project/signup.js
@@ -0,0 +1,31 @@
+
+
+
+
+let form = document.querySelector("form")
+
+form.addEventListener("submit",(e)=>{
+
+    e.preventDefault()
+
+    let useremail = document.getElementById("useremail").value
+    let username = document.getElementById("username").value
+    let userpass = document.getElementById("userpass").value
+
+    console.log(useremail, username , userpass)
+
+    localStorage.setItem("signupEmail",useremail )
+    localStorage.setItem("signupName",username)
+    localStorage.setItem("signupPass",userpass)
+
+    alert("signup done successfully")
+})
+
+
+
+
+// localStorage.setItem(key , value )
+// localStorage.getItem("key")
+// localStorage.removeItem("key")
+// localStorage.clear()
+
